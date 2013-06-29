075a4d8abf721a43fcab333113735bf2986f35b0
Glen Langer <contao.modul@glen-langer.de>
2013-06-29 22:11:00 +0200

    Übernahme der original Änderungen, Offset Korrekturen

--- system/modules/grid_16_columns/languages/en/tl_layout.php
+++ system/modules/grid_16_columns/languages/en/tl_layout.php
@@ -11,7 +11,7 @@
  * @see        https://github.com/BugBuster1701/contao_grid_16_columns
  */
 
-$GLOBALS['TL_LANG']['tl_layout']['responsive-1140-16-percent.css'][0] = 'Responsive grid, percent, 1140px-16';
+$GLOBALS['TL_LANG']['tl_layout']['responsive-1140-16-percent.css'][0] = '16-column responsive grid, percent, 1140px';
 $GLOBALS['TL_LANG']['tl_layout']['responsive-1140-16-percent.css'][1] = 'Adds a responsive 16-column percental grid with 1140px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".'; 
-$GLOBALS['TL_LANG']['tl_layout']['responsive-1120-16-pixel.css'][0] = 'Responsive grid, pixel, 1120px-16';
+$GLOBALS['TL_LANG']['tl_layout']['responsive-1120-16-pixel.css'][0] = '16-column responsive grid, pixel, 1120px';
 $GLOBALS['TL_LANG']['tl_layout']['responsive-1120-16-pixel.css'][1] = 'Adds a responsive 16-column pixel grid with 1120px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".'; 
