4e419047bcd64b3cc45de8e9269ef0f1cf2dd6d1
Glen Langer <contao.modul@glen-langer.de>
2015-05-11 12:56:18 +0200

    Update tl_layout.php
    Hotfix #16, language variable names. Update in transifex comes later.

--- system/modules/grid_16_columns/languages/en/tl_layout.php
+++ system/modules/grid_16_columns/languages/en/tl_layout.php
@@ -12,12 +12,12 @@
  */
 
 
-$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive']['css']['0']   = '16-column  grid, pixel, 1120px, responsive part';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive']['css']['1']   = 'Responsive part for the 16-column grid, pixel, 1120px';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel']['css']['0']              = '16-column  grid, pixel, 1120px';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel']['css']['1']              = 'Adds a 16-column pixel grid with 1120px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive']['css']['0'] = '16-column grid, percent, 1140px, responsive part';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive']['css']['1'] = 'Responsive part for the 16-column grid, percent, 1140px';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent']['css']['0']            = '16-column grid, percent, 1140px';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent']['css']['1']            = 'Adds a 16-column percental grid with 1140px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['0']   = '16-column  grid, pixel, 1120px, responsive part';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['1']   = 'Responsive part for the 16-column grid, pixel, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['0']              = '16-column  grid, pixel, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['1']              = 'Adds a 16-column pixel grid with 1120px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['0'] = '16-column grid, percent, 1140px, responsive part';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['1'] = 'Responsive part for the 16-column grid, percent, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['0']            = '16-column grid, percent, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['1']            = 'Adds a 16-column percental grid with 1140px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
 
