97a5d68b48084f9daa9c1bc63a611721762d3bdc
Glen Langer <contao.modul@glen-langer.de>
2016-03-09 23:26:24 +0100

    Update Sprachdateien (hi,fa)

---
+++ system/modules/grid_16_columns/languages/fa/modules.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-grid_16_columns/language/fa/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2016-03-09T23:25:15+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['grid_16_columns']['0'] = 'شبکه‌ی کانتائو با ۱۶ ستون';
+
---
+++ system/modules/grid_16_columns/languages/fa/tl_layout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-grid_16_columns/language/fa/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2016-03-09T23:25:16+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['0'] = '16-column  grid, pixel, 1120px';
+
